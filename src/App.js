c9e02258457718991c7488c1f71d72f6f14b36c4
Ashhad Ghazi <ashhad.ghazi@gmail.com>
2019-10-24 15:37:27 -0500

    added new images and updated css

--- src/App.js
+++ src/App.js
@@ -22,31 +22,55 @@ class App extends React.Component {
             <div className="widget-layout">
               <div className="widget widgetTeal" onClick={() =>
                   {this.props.toggleState('allState', this.props.allState)}}>
-                <div className="deviceImg"><img className="deviceImgSize" src={require('./images/lb.png')} /></div>
+                <div className="deviceImg">
+                  <img className="deviceImgSize"
+                       src={this.props.allState ? require('./images/lb_on.png') : require('./images/lb_off.png')} />
+                  <div className="subheader">All Lights</div>
+                </div>
               </div>
               <div className="widget widgetGreen" onClick={() =>
                   {this.props.toggleState('bedroomState', this.props.bedroomState)}}>
-                <div className="deviceImg"><img className="deviceImgSize" src={require('./images/lb.png')} /></div>
+                <div className="deviceImg">
+                  <img className="deviceImgSize"
+                       src={this.props.bedroomState ? require('./images/lb_on.png') : require('./images/lb_off.png')} />
+                  <div className="subheader">Bedroom Lights</div>
+                </div>
               </div>
             </div>
             <div className="widget-layout">
               <div className="widget widgetPurple" onClick={() =>
                   {this.props.toggleState('kitchenState', this.props.kitchenState)}}>
-                <div className="deviceImg"><img className="deviceImgSize" src={require('./images/lb.png')} /></div>
+                <div className="deviceImg">
+                  <img className="deviceImgSize"
+                       src={this.props.kitchenState ? require('./images/lb_on.png') : require('./images/lb_off.png')} />
+                  <div className="subheader">Kitchen Lights</div>
+                </div>
               </div>
               <div className="widget widgetYellow" onClick={() =>
                   {this.props.toggleState('diningState', this.props.diningState)}}>
-                <div className="deviceImg"><img className="deviceImgSize" src={require('./images/lb.png')} /></div>
+                <div className="deviceImg">
+                  <img className="deviceImgSize"
+                       src={this.props.diningState ? require('./images/lb_on.png') : require('./images/lb_off.png')} />
+                  <div className="subheader-long">Dining Room Lights</div>
+                </div>
               </div>
             </div>
             <div className="widget-layout">
               <div className="widget widgetGrey" onClick={() =>
                   {this.props.toggleState('livingState', this.props.livingState)}}>
-                <div className="deviceImg"><img className="deviceImgSize" src={require('./images/lb.png')} /></div>
+                <div className="deviceImg">
+                  <img className="deviceImgSize"
+                       src={this.props.livingState ? require('./images/lb_on.png') : require('./images/lb_off.png')} />
+                  <div className="subheader-long">Living Room Lights</div>
+                </div>
               </div>
               <div className="widget widgetBlue" onClick={() =>
                   {this.props.toggleState('hallwayState', this.props.hallwayState)}}>
-                <div className="deviceImg"><img className="deviceImgSize" src={require('./images/lb.png')} /></div>
+                <div className="deviceImg">
+                  <img className="deviceImgSize"
+                       src={this.props.hallwayState ? require('./images/lb_on.png') : require('./images/lb_off.png')} />
+                  <div className="subheader">Hallway Lights</div>
+                </div>
               </div>
             </div>
         </div>
